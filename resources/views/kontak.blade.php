47fb8ed4ea0455e41254d15e5c287d6bb9f44312
pachil-hyper <fazrilfadilah2580@gmail.com>
2025-10-03 10:32:29 +0700

    Prak 3

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tentang Kami - LaraPress</title>
+</head>
+<body>
+    <h1>Kontak Tentang Fazril Fadilah</h1>
+    <p>Email : fazrilfadilah2580@gmail.com</p>
+    <p>Nomor Telp : 089691649697</p>
+</body>
+</html>
+
+<a href="/">Kembali ke Halaman Utama</a>
